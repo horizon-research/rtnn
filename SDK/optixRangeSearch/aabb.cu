914f789950cb9a0213d9b3dc0500110d406ee37b
Yuhao Zhu <yzhu67@node2x8a.cs.rochester.edu>
2021-07-13 08:38:32 -0400

    commit aabb.cu.

---
+++ SDK/optixRangeSearch/aabb.cu
@@ -0,0 +1,38 @@
+#include <optix.h>
+#include <sutil/vec_math.h>
+
+__global__ void kGenAABB_t (
+      const float3* points,
+      float radius,
+      unsigned int N,
+      CUdeviceptr d_aabb
+)
+{
+  unsigned int particleIndex = blockIdx.x * blockDim.x + threadIdx.x;
+  if (particleIndex >= N) return;
+
+  float3 center = points[particleIndex];
+
+  float3 m_min = center - radius;
+  float3 m_max = center + radius;
+
+  OptixAabb *aabb = reinterpret_cast<OptixAabb*>(d_aabb);
+
+  aabb[particleIndex] =
+  {
+    m_min.x, m_min.y, m_min.z,
+    m_max.x, m_max.y, m_max.z
+  };
+}
+
+void kGenAABB(float3* points, float radius, unsigned int numPrims, CUdeviceptr d_aabb, cudaStream_t stream) {
+  unsigned int threadsPerBlock = 64;
+  unsigned int numOfBlocks = numPrims / threadsPerBlock + 1;
+
+  kGenAABB_t <<<numOfBlocks, threadsPerBlock, 0, stream>>> (
+      points,
+      radius,
+      numPrims,
+      d_aabb
+     );
+}
