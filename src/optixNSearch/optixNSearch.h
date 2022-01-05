f59193e75b5abdcd9ded6903f068480a86f1533a
rochester <rochester@node2x10d.cs.rochester.edu>
2022-01-05 12:04:42 -0500

    clean up optixNSearch.h.

--- src/optixNSearch/optixNSearch.h
+++ src/optixNSearch/optixNSearch.h
@@ -37,17 +37,12 @@ enum ParticleType
     QUERY = 1
 };
 
+// TODO: really only need 1.
 enum RayType
 {
     RAY_TYPE_RADIANCE  = 0,
     RAY_TYPE_OCCLUSION = 1,
     RAY_TYPE_COUNT
-};
-
-struct BasicLight
-{
-    float3  pos;
-    float3  color;
 };
 
 enum SearchType
@@ -70,135 +65,14 @@ struct Params
     OptixTraversableHandle handle;
 };
 
-
-struct CameraData
-{
-    float3       eye;
-    float3       U;
-    float3       V;
-    float3       W;
-};
-
-
 struct MissData
 {
-    float3 bg_color;
 };
-
-
-struct Sphere
-{
-    float3	center;
-    float 	radius;
-};
-
 
 struct GeomData
 {
-  float3 *spheres;
 };
-
-
-enum SphereShellHitType {
-    HIT_OUTSIDE_FROM_OUTSIDE = 1u << 0,
-    HIT_OUTSIDE_FROM_INSIDE  = 1u << 1,
-    HIT_INSIDE_FROM_OUTSIDE  = 1u << 2,
-    HIT_INSIDE_FROM_INSIDE   = 1u << 3
-};
-
-
-struct SphereShell
-{
-	float3 	center;
-	float 	radius1;
-	float 	radius2;
-};
-
-
-struct Parallelogram
-{
-    Parallelogram() = default;
-    Parallelogram( float3 v1, float3 v2, float3 anchor ):
-    v1( v1 ), v2( v2 ), anchor( anchor )
-    {
-        float3 normal = normalize(cross( v1, v2 ));
-        float d = dot( normal, anchor );
-        this->v1 *= 1.0f / dot( v1, v1 );
-        this->v2 *= 1.0f / dot( v2, v2 );
-        plane = make_float4(normal, d);
-    }
-    float4	plane;
-    float3 	v1;
-    float3 	v2;
-    float3 	anchor;
-};
-
-
-struct Phong
-{
-    float3 Ka;
-    float3 Kd;
-    float3 Ks;
-    float3 Kr;
-    float  phong_exp;
-};
-
-
-struct Glass
-{
-    float  importance_cutoff;
-    float3 cutoff_color;
-    float  fresnel_exponent;
-    float  fresnel_minimum;
-    float  fresnel_maximum;
-    float  refraction_index;
-    float3 refraction_color;
-    float3 reflection_color;
-    float3 extinction_constant;
-    float3 shadow_attenuation;
-    int    refraction_maxdepth;
-    int    reflection_maxdepth;
-};
-
-
-struct CheckerPhong
-{
-    float3 Kd1, Kd2;
-    float3 Ka1, Ka2;
-    float3 Ks1, Ks2;
-    float3 Kr1, Kr2;
-    float  phong_exp1, phong_exp2;
-    float2 inv_checker_size;
-};
-
 
 struct HitGroupData
 {
-    union
-    {
-        Sphere          sphere;
-        SphereShell     sphere_shell;
-        Parallelogram   parallelogram;
-    } geometry;
-
-    union
-    {
-        Phong           metal;
-        Glass           glass;
-        CheckerPhong    checker;
-    } shading;
-};
-
-
-struct RadiancePRD
-{
-    float3 result;
-    float  importance;
-    int    depth;
-};
-
-
-struct OcclusionPRD
-{
-    float3 attenuation;
 };
